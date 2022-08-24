ad21dc03974ff2174d7e55e197a6a201579c5a50
김시환 <htogether7@gmail.com>
2022-08-24 18:03:51 +0900

    0824

--- brute-force/1025.py
+++ brute-force/1025.py
@@ -6,100 +6,25 @@ n,m = map(int, input().split());
 
 board = [list(map(int, list(input().rstrip()))) for _ in range(n)];
 
-dy = [i for i in range(-n+1,n)];
-dx = [i for i in range(m)];
+dys = [i for i in range(-n,n)];
+dxs = [i for i in range(-m,m)];
 max_result = -1;
-# print(dy);
-for i in dy:
-    for j in range(len(dx)):
-        # print(i,j);
-        if i == 0 and j == 0:
-            for y in range(n):
-                for x in range(m):
-                    if int(math.sqrt(board[y][x])) == math.sqrt(board[y][x]):
-                        max_result = max(max_result, board[y][x]);
-        else:
-            if i >= 0:
-                for y in range(0,n-i):
-                    for x in range(0,m-j):
-                        # print(y,x,i,j);
-                        tmp = [];
-                        # tmp.append((y,x));
-                        now_y = y;
-                        now_x = x;
-                        while now_y < n and now_x < m:
-                            tmp.append((now_y,now_x));
-                            now_y += i;
-                            now_x += j;
-                        # print(i,j);
-                        # print(tmp);
-                        string_tmp = [];
-                        for pos in tmp:
-                            string_tmp.append(board[pos[0]][pos[1]]);
-                        if len(string_tmp) != 0:
-                            # print(string_tmp, i, j);
-                            # result = ("".join(string_tmp))
-                            result = 0;
-                            for l in range(len(string_tmp)):
-                                result += string_tmp[l] * (10 ** (len(string_tmp)-1-l));
-                            # print(string_tmp);
-                            # print(result**(1/2));
-                            if int(math.sqrt(result)) == math.sqrt(result):
-                                max_result = max(max_result, result);
-                                # print(result);
-                            string_tmp.reverse();
-                            result = 0;
-                            for l in range(len(string_tmp)):
-                                result += string_tmp[l] * (10 ** (len(string_tmp)-1-l));
-                            if int(math.sqrt(result)) == math.sqrt(result):
-                                max_result = max(max_result, result);
-                            # print(result);
-            else:
-                # print(i,j);
-                for y in range(n-1, -i-1, -1):
-                    for x in range(0,m-j):
-                        # print(y,x);
-                        tmp = [];
-                        # tmp.append((y,x));
-                        now_y = y;
-                        now_x = x;
-                        while 0 <= now_y < n and now_x < m:
-                            tmp.append((now_y,now_x));
-                            now_y += i;
-                            now_x += j;
-                        # print(i,j);
-                        # print(tmp);
-                        string_tmp = [];
-                        for pos in tmp:
-                            string_tmp.append(board[pos[0]][pos[1]]);
-                        if len(string_tmp) != 0:
-                            # print(string_tmp, i, j);
-                            # result = ("".join(string_tmp))
-                            result = 0;
-                            for l in range(len(string_tmp)):
-                                result += string_tmp[l] * (10 ** (len(string_tmp)-1-l));
-                            # print(string_tmp);
-                            # print(result**(1/2));
-                            if int(math.sqrt(result)) == math.sqrt(result):
-                                max_result = max(max_result, result);
-                                # print(result);
-                            string_tmp.reverse();
-                            result = 0;
-                            for l in range(len(string_tmp)):
-                                result += string_tmp[l] * (10 ** (len(string_tmp)-1-l));
-                            if int(math.sqrt(result)) == math.sqrt(result):
-                                max_result = max(max_result, result);
 
-                    # print(string_tmp);
-                    # print(result**(1/2));
-
-                    # print("".join)
-# if max_result == 0:
-#     print(-1);
-# else:
+for y in range(n):
+    for x in range(m):
+        for dy in dys:
+            for dx in dxs:
+                if dy == 0 and dx == 0:
+                    continue;
+                # print(y,x,dy,d    x)
+                now_y = y;
+                now_x = x;
+                tmp = [];
+                while 0<= now_y < n and 0 <= now_x < m:
+                    tmp.append(str(board[now_y][now_x]));
+                    now_y += dy;
+                    now_x += dx;
+                    if int(math.sqrt(int("".join(tmp)))) == math.sqrt(int("".join(tmp))):
+                        max_result = max(max_result, int("".join(tmp)));
+                # print(tmp);
 print(max_result);
-
-# for j in range(1, len(dx)):
-
-
-# print(board);
---
+++ graph/14502.py
@@ -0,0 +1,96 @@
+import sys;
+from collections import deque
+from tabnanny import check;
+input = sys.stdin.readline;
+
+n, m = map(int, input().rstrip().split());
+
+board = [list(map(int, input().rstrip().split()))for _ in range(n)];
+
+board_copy = [board[i][::] for i in range(n)];
+
+zeros = [];
+max_safe_zone = 0;
+for y in range(n):
+    for x in range(m):
+        if board[y][x] == 0:
+            zeros.append((y,x));
+
+# print(zeros);
+
+dx = [1,-1,0,0,];
+dy = [0,0,1,-1];
+
+count = 0;
+
+def dfs(count):
+    global max_safe_zone;
+    if count == 3:
+        tmp_count = 0;
+        tmp_board = [board[i][::] for i in range(n)];
+        check_break = False;
+        for y in range(n):
+            for x in range(m):
+                if tmp_board[y][x] == 2:
+                    q = deque([(y,x)]);
+                    while len(q) != 0:
+                        next = q.popleft();
+                    #     # print(q);
+                        for ind in range(4):
+                            if 0 <= next[0] + dy[ind] < n and 0 <= next[1] + dx[ind] < m and tmp_board[next[0]+dy[ind]][next[1]+dx[ind]] == 0:
+                                q.append((next[0] + dy[ind], next[1] + dx[ind]));
+                                tmp_board[next[0] + dy[ind]][next[1] + dx[ind]] = 2;
+                                tmp_count += 1;
+        max_safe_zone = max(max_safe_zone, len(zeros) - 3 - tmp_count)
+        return;
+
+    for y in range(n):
+        for x in range(m):
+            if board[y][x] == 0:
+                board[y][x] = 1;
+                dfs(count+1);
+                board[y][x] = 0;
+
+dfs(0);
+
+# for i in range(len(zeros)-2):
+#     for j in range(1, len(zeros)-1):
+#         for k in range(2, len(zeros)):
+#             board[zeros[i][0]][zeros[i][1]] = 1;
+#             board[zeros[j][0]][zeros[j][1]] = 1;
+#             board[zeros[k][0]][zeros[k][1]] = 1;
+#             # print(board);
+#             tmp_count = 0;
+#             check_break = False;
+#             for y in range(n):
+#                 for x in range(m):
+#                     if board[y][x] == 2:
+#                         q = deque([(y,x)]);
+#                         while len(q) != 0:
+#                             next = q.popleft();
+#                         #     # print(q);
+#                             for ind in range(4):
+#                                 if 0 <= next[0] + dy[ind] < n and 0 <= next[1] + dx[ind] < m and board[next[0]+dy[ind]][next[1]+dx[ind]] == 0:
+#                                     q.append((next[0] + dy[ind], next[1] + dx[ind]));
+#                                     board[next[0] + dy[ind]][next[1] + dx[ind]] = 2;
+#                                     tmp_count += 1;
+#                                     if tmp_count == len(zeros) - 3:
+#                                         check_break = True;
+#                                         break;
+#                             if check_break:
+#                                 break;
+#                     if check_break:
+#                         break;
+#                 if check_break:
+#                     break;
+#                         # print("finish");
+#             # for y in range(n):
+#             #     for x in range(m):
+#             #         if board[y][x] == 0:
+#             #             tmp_count += 1;
+#             # max_safe_zone = max(tmp_count, max_safe_zone)
+#             # print(max_safe_zone)
+#             max_safe_zone = max(max_safe_zone, len(zeros) - 3 - tmp_count)
+#             board = [board_copy[s][::] for s in range(n)];
+
+print(max_safe_zone);
