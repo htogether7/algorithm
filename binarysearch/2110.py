0ae0742b4959c96e2e72f9bb195f727a42cb511f
htogether7 <htogether7@gmail.com>
2023-01-10 11:05:55 +0900

    0110

--- binarysearch/2110.py
+++ binarysearch/2110.py
@@ -5,15 +5,41 @@ n, c = map(int, input().split());
 
 board = [int(input()) for _ in range(n)];
 
-board.sort();
+board.sort()
 
-start = board[0];
-end = board[-1];
+l = 0
+r = board[-1]-1
+answer =  0
+while l <= r:
+    mid = (l+r) // 2
+    tmp_count = 1
+    pos = board[0]
+    for i in range(1, len(board)):
+        if board[i] - pos >= mid:
+            tmp_count += 1
+            pos = board[i]
+    if tmp_count < c:
+        r = mid-1
+    elif tmp_count >= c:
+        l = mid+1
+        answer = max(answer,mid)
+    # print(mid, tmp_count, pos)
+    # breakㅂ
+print(answer)
 
-min_length = end-start;
-c -= 2;
-while c:
-    mid = (start + end) // 2;
+# print(board)
+# if c == 2:
+    # print(board[-1] - board[0])
+# else:
+    
+# print(board)
+# start = board[0];
+# end = board[-1];
+
+# min_length = end-start;
+# c -= 2;
+# while c:
+    # mid = (start + end) // 2;
 
 
-print(board);
+# print(board);
--- dp/12865.py
+++ dp/12865.py
@@ -7,17 +7,35 @@ board = []
 
 for _ in range(n):
     board.append(list(map(int,input().split())));
-
+board.append([1,0])
 board.sort(key = lambda x : x[0]);
 # print(board);
 
-dp = [0] * (k+1)
-for ind in range(k+1):
-    for w,v in board:
-        # print(w,v);
-        if ind + w < k+1:
-            dp[ind+w] = max(dp[ind+w], dp[ind] + v);
-
-# print(max(dp));
-print(dp[-1]);
+dp = [[0] * (k+1) for _ in range(n+1)]
 # print(dp)
+# tmp_max = 0
+# for ind in range(k+1):
+    # for w,v in board:
+# for c in range(1, k+1):
+    # for r in range(1, n+1):
+        # dp[r][c] = max(dp[r][c-1], )
+        # if r == 1:
+            # f
+#         # print(w,v);
+#         if ind + w < k+1:
+#             dp[ind+w] = max(dp[ind+w], dp[ind] + v);
+#             # tmp_max = max(tmp_max,dp[ind+w])
+#             # dp[ind+w] = max(dp[ind+w], tmp_max)
+            
+#     # print(dp, ind)
+# # print(max(dp));
+# print(dp[-1]);
+# print(dp)
+for r in range(n+1):
+    for c in range(1,k+1):
+        if c - board[r][0] >= 0:
+            dp[r][c] = max(dp[r-1][c], dp[r-1][c-board[r][0]] + board[r][1])
+        else:
+            dp[r][c] = dp[r-1][c]
+print(dp[-1][-1])
+        # dp[r][c] = max(dp[r-1][c])
