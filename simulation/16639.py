13fc8b69a7b04489c04dfe9332d42f078f9e0714
htogether7 <htogether7@gmail.com>
2023-05-04 00:33:49 +0900

    16639

--- simulation/16639.py
+++ simulation/16639.py
@@ -26,38 +26,20 @@ def cal(a,b,c):
 
 dp = [[[0,0] for _ in range(len(nums))]  for _ in range(len(nums))]
 
-for i in range(len(nums)-1):
-    dp[i][i+1][0] = cal(nums[i], nums[i+1], cals[i])
-    dp[i][i+1][1] = cal(nums[i], nums[i+1], cals[i])
-
-for i in range(2,len(nums)):
-    for j in range(len(nums)-i):
-        for k in range(i,j):
-            dp[j][j+i][0] = max(cal(dp[j][j+i-1][0], nums[j+i], cals[j+i-1]),
-                            cal(dp[j][j+i-1][1], nums[j+i], cals[j+i-1]), 
-                            cal(nums[j],dp[j+1][j+i][0], cals[j]),
-                            cal(nums[j],dp[j+1][j+i][1], cals[j])
-                            )
-            dp[j][j+i][1] = min(cal(dp[j][j+i-1][0], nums[j+i], cals[j+i-1]),
-                            cal(dp[j][j+i-1][1], nums[j+i], cals[j+i-1]), 
-                            cal(nums[j],dp[j+1][j+i][0], cals[j]),
-                            cal(nums[j],dp[j+1][j+i][1], cals[j])
-                            )
 
 for i in range(len(nums)):
     for j in range(len(nums)-i):
         if i == 0:
-            dp[i][j][0] = nums[i]
-            dp[i][j][1] = nums[i]
+            dp[j][j][0] = nums[j]
+            dp[j][j][1] = nums[j]
         else:
             arr = []
             for k in range(j,j+i):
-                arr.append(cal(dp[j][k][0], dp[]))
-                # dp[j][j+i][0] = 
-                # dp[j][j+i][1] = 
+                arr.append(cal(dp[j][k][0], dp[k+1][j+i][0], cals[k]))
+                arr.append(cal(dp[j][k][1], dp[k+1][j+i][0], cals[k]))
+                arr.append(cal(dp[j][k][0], dp[k+1][j+i][1], cals[k]))
+                arr.append(cal(dp[j][k][1], dp[k+1][j+i][1], cals[k]))
+            dp[j][j+i][0] = max(arr)
+            dp[j][j+i][1] = min(arr)
 
-
-
-print(dp)
-# print(answer)
-
+print(dp[0][len(nums)-1][0])
