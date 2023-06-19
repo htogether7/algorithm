05476647b065ff669e0fba2b2a4a03129243e670
htogether7 <htogether7@gmail.com>
2023-06-19 13:32:50 +0900

    15686_dfs

--- simulation/15686.py
+++ simulation/15686.py
@@ -9,6 +9,7 @@ answer = float('inf')
 
 house = []
 chicken = []
+path = []
 
 for i in range(n):
     for j in range(n):
@@ -17,13 +18,32 @@ for i in range(n):
         if board[i][j] == 2:
             chicken.append((i,j))
 
-for comb in combinations(chicken,m):
-    tmp = 0
-    for (i,j) in house:
-        min_distance = float('inf')
-        for (r,c) in comb:
-            min_distance = min(min_distance, abs(i-r) + abs(j-c))
-        tmp += min_distance
-    answer = min(answer, tmp)
+def dfs(l,s):
+    global answer
+    if l == m:
+        tmp = 0
+        for (i,j) in house:
+            min_distance = float('inf')
+            for k in path:
+                min_distance = min(min_distance, abs(i-chicken[k][0]) + abs(j-chicken[k][1]))
+            tmp += min_distance
+        answer = min(answer, tmp)
+        return
+
+    for i in range(s,len(chicken)):
+        path.append(i)
+        dfs(l+1,i+1)
+        path.pop()
+
+dfs(0,0)
 print(answer)
+# for comb in combinations(chicken,m):
+#     tmp = 0
+#     for (i,j) in house:
+#         min_distance = float('inf')
+#         for (r,c) in comb:
+#             min_distance = min(min_distance, abs(i-r) + abs(j-c))
+#         tmp += min_distance
+#     answer = min(answer, tmp)
+# print(answer)
 
